e4b619fdb4041f9e04347166ff5b5d9e70dc180b
eva <me@example.com>
2020-05-18 11:17:15 +0200

    added info text for chatapp, added show more btns and JS toggle logic

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,27 @@ function toggleMenu() {
 let hamburger = document.querySelector('.hamburger');
 
 hamburger.addEventListener('click', toggleMenu);
+
+
+// toggle read more chatapp
+
+function toggleChatapp() {
+  let readChatapp = document.querySelector('.read-more-chatapp');
+  readChatapp.classList.toggle('visible');
+}
+
+let chatappBtn = document.querySelector('.btn-toggle-chatapp');
+
+chatappBtn.addEventListener('click', toggleChatapp);
+
+// toggle read more myflix
+
+function toggleMyflix() {
+  let readMyflix = document.querySelector('.read-more-myflix');
+  readMyflix.classList.toggle('visible');
+}
+
+let myflixBtn = document.querySelector('.btn-toggle-myflix');
+
+myflixBtn.addEventListener('click', toggleMyflix);
+
