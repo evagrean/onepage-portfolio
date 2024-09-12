856508be1d37f3d434f97240278216e44803047c
eva <me@example.com>
2024-09-12 10:54:19 +0200

    updated about text and projects, added images

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,77 @@
 // Toggle menu on small screens
 
 function toggleMenu() {
-  let menu = document.querySelector('.menu-container');
-  menu.classList.toggle('visible');
+  let menu = document.querySelector(".menu-container");
+  menu.classList.toggle("visible");
 }
 
-let hamburger = document.querySelector('.hamburger');
+let hamburger = document.querySelector(".hamburger");
 
-hamburger.addEventListener('click', toggleMenu);
+hamburger.addEventListener("click", toggleMenu);
 
 // Hide mobile menu after a menu item has been clicked
 
 function hideMenu() {
-  let menu = document.querySelector('.menu-container');
-  if (menu.classList.contains('visible')) {
-    menu.classList.remove('visible');
+  let menu = document.querySelector(".menu-container");
+  if (menu.classList.contains("visible")) {
+    menu.classList.remove("visible");
   }
 }
 
-menuList = document.querySelector('.menu');
-menuList.addEventListener('click', hideMenu);
+menuList = document.querySelector(".menu");
+menuList.addEventListener("click", hideMenu);
 
 // Toggle read more chatapp
 
 function readMoreChatapp() {
-  let moreChatapp = document.querySelector('.read-more-chatapp');
-  let chatappBtn = document.querySelector('.btn-toggle-chatapp');
-  moreChatapp.classList.toggle('visible');
+  let moreChatapp = document.querySelector(".read-more-chatapp");
+  let chatappBtn = document.querySelector(".btn-toggle-chatapp");
+  moreChatapp.classList.toggle("visible");
 
-  if (moreChatapp.classList.contains('visible')) {
-    chatappBtn.innerHTML = 'Read less';
+  if (moreChatapp.classList.contains("visible")) {
+    chatappBtn.innerHTML = "Read less";
   } else {
-    chatappBtn.innerHTML = 'Read more';
+    chatappBtn.innerHTML = "Read more";
   }
 }
 
-let chatappBtn = document.querySelector('.btn-toggle-chatapp');
+let chatappBtn = document.querySelector(".btn-toggle-chatapp");
 
-chatappBtn.addEventListener('click', readMoreChatapp);
+chatappBtn.addEventListener("click", readMoreChatapp);
 
 // Toggle read more myflix
 
-function readMoreMyflix() {
-  let moreMyflix = document.querySelector('.read-more-myflix');
-  let myflixBtn = document.querySelector('.btn-toggle-myflix');
-  moreMyflix.classList.toggle('visible');
+// function readMoreMyflix() {
+//   let moreMyflix = document.querySelector(".read-more-myflix");
+//   let myflixBtn = document.querySelector(".btn-toggle-myflix");
+//   moreMyflix.classList.toggle("visible");
 
-  if (moreMyflix.classList.contains('visible')) {
-    myflixBtn.innerHTML = 'Read less';
-  } else {
-    myflixBtn.innerHTML = 'Read more';
-  }
+//   if (moreMyflix.classList.contains("visible")) {
+//     myflixBtn.innerHTML = "Read less";
+//   } else {
+//     myflixBtn.innerHTML = "Read more";
+//   }
+// }
+
+// let myflixBtn = document.querySelector(".btn-toggle-myflix");
+
+// myflixBtn.addEventListener("click", readMoreMyflix);
+
+// Toggle see more projects
+function seeMoreProjects() {
+  let moreProjects = document.querySelector(".read-more-projects");
+  moreProjects.classList.toggle("visible");
 }
 
-let myflixBtn = document.querySelector('.btn-toggle-myflix');
-
-myflixBtn.addEventListener('click', readMoreMyflix);
+let moreProjectsBtn = document.querySelector(".btn-toggle-projects");
+moreProjectsBtn.addEventListener("click", seeMoreProjects);
 
 // Add active class to currently clicked menu-item
-let menuItems = document.querySelectorAll('.menu-item');
+let menuItems = document.querySelectorAll(".menu-item");
 
-menuItems.forEach(menuItem => {
-  menuItem.addEventListener('click', function () {
-    menuItems.forEach(item => item.classList.remove('active'));
-    this.classList.add('active');
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", function () {
+    menuItems.forEach((item) => item.classList.remove("active"));
+    this.classList.add("active");
   });
 });
-
